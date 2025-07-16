d8728dd2f8f5af714daf563d4a0519593cc0ea56
Bethel Amadi <amadibethel@icloud.com>
2025-07-17 00:18:05 +0200

    Add file for Project Setup

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 text-center p-4">
+      <p className="text-sm">© 2025 My App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
